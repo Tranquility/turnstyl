ff03a00de220b5742c16dd3adf3bf50c262642f2
Ole Reifschneider <mail@ole-reifschneider.de>
2013-06-14 17:37:30 +0200

    Update keys only if config has changed

--- lib/sshatar/client/client.rb
+++ lib/sshatar/client/client.rb
@@ -1,15 +1,34 @@
+require 'toml'
+
 module Sshatar
   class Client
     def self.home_folder
       File.expand_path("~/")
     end
 
-    def update_authorized_keys people=[]
+    def initialize
+      @config_path = File.expand_path(Client.home_folder+".sshatar_config")
+    end
+
+
+    def run
+      if File.mtime(@config_path) > File.mtime(Client.home_folder+".ssh/authorized_keys")
+        update_authorized_keys
+      end
+    end
+
+    def load_settings
+      TOML.load_file(@config_path)
+    end
+
+    def update_authorized_keys
+      settings = load_settings
+      authorized_users = settings["userlist"]
       keys = []
-      if people.empty?
+      if authorized_users.empty?
         keys << '# YOU HAVE NOT AUTHORIZED ANYONE TO LOGIN'
       else
-        people.each do |person|
+        authorized_users.each do |person|
           keys = keys + Communicator.new.get_keys_from(person)
         end
       end
--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -4,7 +4,8 @@ require_relative '../../lib/sshatar/client'
 module Sshatar
   describe Client do
     let(:dummy_home) { base_path+'/spec/dummy_home_folder/' }
-    let(:file) { dummy_home+'/.ssh/authorized_keys' }
+    let(:key_file) { dummy_home+'.ssh/authorized_keys' }
+    let(:config_file) { dummy_home+'.sshatar_config' }
     let(:sshatar_client) {Client.new}
 
     before do
@@ -17,33 +18,45 @@ module Sshatar
     end
 
     it 'creates authorized_keys file' do
-      File.exist?(file).should be_false
+      sshatar_client.should_receive(:load_settings).and_return("userlist" => [])
 
+      File.exist?(key_file).should be_false
       sshatar_client.update_authorized_keys
-
-      File.exist?(file).should be_true
+      File.exist?(key_file).should be_true
     end
 
-    it 'overwrites last authorized_keys file' do
-      sshatar_client.update_authorized_keys
-      last_time = File.mtime(file)
-
-      sleep 0.0002
-      sshatar_client.update_authorized_keys
-      File.mtime(file).should_not eq last_time
-    end
 
     it 'adds a comment when there are no authorized keys' do
+      sshatar_client.should_receive(:load_settings).and_return("userlist" => [])
       sshatar_client.update_authorized_keys
 
-      File.read(file).should eq "# YOU HAVE NOT AUTHORIZED ANYONE TO LOGIN\n"
+      File.read(key_file).should eq "# YOU HAVE NOT AUTHORIZED ANYONE TO LOGIN\n"
     end
 
     it 'starts downloading the keys if there are user in the config file' do
       Communicator.any_instance.stub(:get_keys_from).and_return(['12345', '67890'])
-      sshatar_client.update_authorized_keys ['flooose']
-      File.read(file).should eq "12345\n67890\n"
+      sshatar_client.should_receive(:load_settings).and_return("userlist" => ['flooose'])
+      sshatar_client.update_authorized_keys
+      File.read(key_file).should eq "12345\n67890\n"
     end
 
+    describe '#run' do
+      it 'does not overwrite authorized_keys file if configuration not changed' do
+        some_time = Time.now
+        File.stub(:mtime).and_return(some_time)
+
+        sshatar_client.should_not_receive(:update_authorized_keys)
+        sshatar_client.run
+      end
+
+      it 'overwrites authorized_keys file if configuration changed' do
+        some_time = Time.now
+        File.should_receive(:mtime).with(key_file).and_return(some_time)
+        File.should_receive(:mtime).with(config_file).and_return(some_time + 2)
+
+        sshatar_client.should_receive(:update_authorized_keys)
+        sshatar_client.run
+      end
+    end
   end
 end
